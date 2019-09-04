218d7e6819682bd49ab9bf7411ad076f0fcead81
Rockwood's Desktop <rockwoodfrank@gmail.com>
2019-09-03 18:20:11 -0600

    WORK

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+import Bound from 'bounds.js/dist/bounds.js'
+
+const boundary = Bound({
+    margins: { bottom: 100 }
+})
+
+const img = document.querySelectorAll('img')
+
+boundary.watch(img, () => {
+    console.log("hey")
+})
