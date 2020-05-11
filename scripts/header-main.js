5f9a598c57779d137e5cce3235630cab767d2028
Rockwood Frank <43915594+rfrank6023@users.noreply.github.com>
2020-05-11 17:21:59 -0600

    Add Olympic background image to contact page

---
+++ scripts/header-main.js
@@ -0,0 +1,31 @@
+let headerMain = {
+    template: `
+    <header data-aos="fade" data-aos-delay="100">
+    <div id="logos">
+      <h4 id="logo">The Law Office of</h4>
+      <h3 id="logo2">Julie A. Brown, Attorney at Law</h3>
+    </div>
+    <img id="logo-justice" src="./images/logo-justice.svg" alt="Picture of Justice">
+    <nav>
+      <a class="navs" href="./index.html">Home</a>
+      <div class="dropdown">
+        <a class="dropbtn navs">Services</a>
+        <div class="dropdown-content">
+          <a href="./divorcechildcustody.html">Divorce and Child Custody</a>
+          <a href="./mediation.html">Mediation</a>
+          <a href="./adoptions.html">Adoptions</a>
+        </div>
+      </div>
+      <a class="navs" href="./contact.html">Contact</a>
+      <a class="navs" href="./about.html">About Us</a>
+    </nav>
+  </header>
+    `
+}
+
+new Vue({
+  el: "#header",
+  components: {
+    'header-main': headerMain,
+  }
+})
